c86be33500c8f2bf9dc91d76fc0ff18a939159a0
VinceWang <v0928062815@gmail.com>
2023-06-04 23:19:28 +0800

    SL改版

--- MapReduce範例/AGM需修改JS/修改後JS檔/EV_SL_GVD_ExportForm.js
+++ MapReduce範例/AGM需修改JS/修改後JS檔/EV_SL_GVD_ExportForm.js
@@ -4,12 +4,12 @@
  * @NModuleScope Public
  */
 
-define(['N/ui/serverWidget', 'N/search', 'N/task'],
+define(['N/ui/serverWidget', 'N/search', 'N/task', './commonAPI/commonUtil', 'N/runtime', 'N/file'],
 	/**
 	 * @param {serverWidget} serverWidget
 	 * @param {search} search
 	 */
-	function (serverWidget, search, task) {
+	function (serverWidget, search, task, util, runtime,file) {
 		/**
 		 * Definition of the Suitelet script trigger point.
 		 *
@@ -22,6 +22,7 @@ define(['N/ui/serverWidget', 'N/search', 'N/task'],
 		function onRequest(context) {
 
 			var req = context.request;
+			var form;
 
 			if (context.request.method === 'GET') {
 				//↓↓↓ 預設稅籍編號 -------------------
@@ -53,7 +54,7 @@ define(['N/ui/serverWidget', 'N/search', 'N/task'],
 				var field_identifier = "";
 				var registration_number = defRegNo;
 
-				var form = serverWidget.createForm({
+				form = serverWidget.createForm({
 					title: formName
 				});
 				form.addSubmitButton({
@@ -202,126 +203,249 @@ define(['N/ui/serverWidget', 'N/search', 'N/task'],
 				context.response.writePage(form);
 				//-----------------------------------
 			} else {
-				// after press button 
-				//接前頁參數
-				//一般媒體申報_條件內容
-				var rgd01 = req.parameters.custpage_gd01;  //稅籍編號
-				var rgd02 = req.parameters.custpage_gd02;  //申報範圍:1進項,2銷項,3進銷項
-				var rgd03 = req.parameters.custpage_gd03;  //申報年度
-				var rgd04 = req.parameters.custpage_gd04;  //申報月份_起
-				var rgd05 = req.parameters.custpage_gd05;  //申報月份_迄
+				try {
+					// after press button 
+					//接前頁參數
+					//一般媒體申報_條件內容
+					var rgd01 = req.parameters.custpage_gd01;  //稅籍編號
+					var rgd02 = req.parameters.custpage_gd02;  //申報範圍:1進項,2銷項,3進銷項
+					var rgd03 = req.parameters.custpage_gd03;  //申報年度
+					var rgd04 = req.parameters.custpage_gd04;  //申報月份_起
+					var rgd05 = req.parameters.custpage_gd05;  //申報月份_迄
+					var mr_scripttaskid = req.parameters.mr_scripttaskid;
+					log.debug("mr_scripttaskid", mr_scripttaskid);
+					log.debug('rgd01', rgd01);
+					log.debug('rgd02', rgd02);
+					log.debug('rgd03', rgd03);
+					log.debug('rgd04', rgd04);
+					log.debug('rgd05', rgd05);
 
-				log.debug('rgd01', rgd01);
-				log.debug('rgd02', rgd02);
-				log.debug('rgd03', rgd03);
-				log.debug('rgd04', rgd04);
-				log.debug('rgd05', rgd05);
+					if (mr_scripttaskid == null) {
+						//呼叫MR
+						form = callMR(context, rgd01, rgd02, rgd03, rgd04, rgd05);
+					} else {
+						form = createStatusForm(mr_scripttaskid);
+					}
+					context.response.writePage(form);
 
 
 
-				var mrTask = task.create({
-					taskType: task.TaskType.MAP_REDUCE
-				});
+					//var fileURL = value;
+					// var fileURL = '無法顯示URL';
+					// //結果回報頁面 ----------------------------------
+					// var form = serverWidget.createForm({
+					// 	title: "媒體檔申報 -- 檔案下載結果 " // + loadScope + ' ' + rgd04 + ' ' + rgd05
+					// });
 
-				mrTask.scriptId = 'customscript_ev_mr_gvd_export';
-				mrTask.deploymentId = 'customdeploy_ev_mr_gvd_export';
-				mrTask.paras = {
-					'rgd01': rgd01,
-					'rgd02': rgd02,
-					'rgd03': rgd03,
-					'rgd04': rgd04,
-					'rgd01': rgd01,
-				};
+					// // ============== 表頭COLUMNS ==============
+					// var fieldgroup_columns = form.addFieldGroup({
+					// 	id: 'custpage_RG1',
+					// 	label: '營業人進/銷項資料檔'
+					// });
 
-				var mrTaskId = mrTask.submit();
-				log.debug("mrTaskId",mrTaskId);
-				var taskOutput = task.checkStatus(mrTaskId).output;
-				log.debug("Status",task.checkStatus(mrTaskId));
-				log.debug("taskOutput",taskOutput);
-				taskOutput.run().each(function(result) {
-					var key = result.key;
-					log.debug("key：",key);
-					var value = result.value;
-					log.debug("value：",value);
-				})
+					// var RA1 = form.addField({
+					// 	id: "custpage_rgd01",
+					// 	label: "稅稽編號",
+					// 	type: serverWidget.FieldType.TEXT,
+					// 	container: "custpage_RG1"
+					// });
+					// RA1.defaultValue = rgd01;
+					// RA1.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
 
-				var fileURL = value;
-				//結果回報頁面 ----------------------------------
-				var form = serverWidget.createForm({
-					title: "媒體檔申報 -- 檔案下載結果 " // + loadScope + ' ' + rgd04 + ' ' + rgd05
-				});
+					// var RA2 = form.addField({
+					// 	id: "custpage_rgd02",
+					// 	label: "申報範圍(進項,銷項,進銷項)",
+					// 	type: serverWidget.FieldType.TEXT,
+					// 	container: "custpage_RG1"
+					// });
+					// var rgd02show = '';
+					// if (rgd02 == '1') { rgd02show = '進項'; }
+					// if (rgd02 == '2') { rgd02show = '銷項'; }
+					// if (rgd02 == '3') { rgd02show = '進銷項'; }
+					// RA2.defaultValue = rgd02show;
+					// RA2.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
 
-				// ============== 表頭COLUMNS ==============
-				var fieldgroup_columns = form.addFieldGroup({
-					id: 'custpage_RG1',
-					label: '營業人進/銷項資料檔'
-				});
+					// var RA3 = form.addField({
+					// 	id: "custpage_rgd03",
+					// 	label: "申報年度",
+					// 	type: serverWidget.FieldType.TEXT,
+					// 	container: "custpage_RG1"
+					// });
+					// RA3.defaultValue = rgd03;
+					// RA3.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
 
-				var RA1 = form.addField({
-					id: "custpage_rgd01",
-					label: "稅稽編號",
-					type: serverWidget.FieldType.TEXT,
-					container: "custpage_RG1"
-				});
-				RA1.defaultValue = rgd01;
-				RA1.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
+					// var RA4 = form.addField({
+					// 	id: "custpage_rgd04",
+					// 	label: "申報月起",
+					// 	type: serverWidget.FieldType.TEXT,
+					// 	container: "custpage_RG1"
+					// });
+					// RA4.defaultValue = rgd04;
+					// RA4.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
 
-				var RA2 = form.addField({
-					id: "custpage_rgd02",
-					label: "申報範圍(進項,銷項,進銷項)",
-					type: serverWidget.FieldType.TEXT,
-					container: "custpage_RG1"
-				});
-				var rgd02show = '';
-				if (rgd02 == '1') { rgd02show = '進項'; }
-				if (rgd02 == '2') { rgd02show = '銷項'; }
-				if (rgd02 == '3') { rgd02show = '進銷項'; }
-				RA2.defaultValue = rgd02show;
-				RA2.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
+					// var RA5 = form.addField({
+					// 	id: "custpage_rgd05",
+					// 	label: "申報月迄",
+					// 	type: serverWidget.FieldType.TEXT,
+					// 	container: "custpage_RG1"
+					// });
+					// RA5.defaultValue = rgd05;
+					// RA5.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
 
-				var RA3 = form.addField({
-					id: "custpage_rgd03",
-					label: "申報年度",
-					type: serverWidget.FieldType.TEXT,
-					container: "custpage_RG1"
-				});
-				RA3.defaultValue = rgd03;
-				RA3.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
-
-				var RA4 = form.addField({
-					id: "custpage_rgd04",
-					label: "申報月起",
-					type: serverWidget.FieldType.TEXT,
-					container: "custpage_RG1"
-				});
-				RA4.defaultValue = rgd04;
-				RA4.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
-
-				var RA5 = form.addField({
-					id: "custpage_rgd05",
-					label: "申報月迄",
-					type: serverWidget.FieldType.TEXT,
-					container: "custpage_RG1"
-				});
-				RA5.defaultValue = rgd05;
-				RA5.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
-
-				var RA8 = form.addField({
-					id: "custpage_rgd08",
-					label: "檔案下載結果",
-					type: serverWidget.FieldType.URL,
-					container: "custpage_RG1"
-				});
-				RA8.defaultValue = fileURL;
-				RA8.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
-				//-----------------------------------	
-				context.response.writePage(form);
-				//-----------------------------------	
-
+					// var RA8 = form.addField({
+					// 	id: "custpage_rgd08",
+					// 	label: "檔案下載結果",
+					// 	type: serverWidget.FieldType.TEXT,
+					// 	container: "custpage_RG1"
+					// });
+					// RA8.defaultValue = fileURL;
+					// RA8.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
+					// //-----------------------------------	
+					// context.response.writePage(form);
+					// //-----------------------------------	
+				} catch (error) {
+					log.debug(error.name, error.message);
+				}
 			}
 		}
 
-		function createForm() { }
+		function callMR(context, rgd01, rgd02, rgd03, rgd04, rgd05) {
+			var form;
+			try {
+				log.debug("Into the callMR Function");
+				var mrTask = task.create({
+					taskType: task.TaskType.MAP_REDUCE
+				});
+				mrTask.scriptId = 'customscript_ev_mr_gvd_export';
+				mrTask.deploymentId = 'customdeploy_ev_mr_gvd_export';
+				mrTask.paras = {
+					'custscript__ev_mr_gvd_export_rgd01': rgd01,
+					'custscript__ev_mr_gvd_export_rgd02': rgd02,
+					'custscript__ev_mr_gvd_export_rgd03': rgd03,
+					'custscript__ev_mr_gvd_export_rgd04': rgd04,
+					'custscript__ev_mr_gvd_export_rgd05': rgd05,
+				};
+				log.debug("paras", mrTask.paras);
+				var mrTaskId = mrTask.submit();
+				log.debug("mrTaskId", mrTaskId);
+
+				log.debug("準備進入 createStatusForm Function");
+				form = createStatusForm(mrTaskId);
+				log.debug("8");
+				return form;
+				
+			} catch (error) {
+				log.error("error", error.name);
+				if (error.name == "MAP_REDUCE_ALREADY_RUNNING") {
+					deploymentCount += 1;
+					form = callMR(context);
+					return form;
+				} else {
+					log.error("各類所得申報檔 目前滿載中");
+					var scriptObj = runtime.getCurrentScript();
+					util.ScriptErrorSendMailToOwner(scriptObj.id, "XXIR009_V2_媒體申報申報檔", "目前產生報表皆在滿載中...請稍後再試");
+					form = serverWidget.createForm({
+						title: '報表產生中...'
+					});
+					var mr_error = form.addField({
+						id: 'mr_error',
+						label: 'mr_error',
+						type: serverWidget.FieldType.TEXT
+					});
+					mr_error.defaultValue = "目前產生報表皆在滿載中...請稍後再試";
+					return form;
+				}
+			}
+		};
+		function createStatusForm(scriptTaskId) {
+			try {
+				var form;
+				//狀態顯示
+				log.debug("scriptTaskId", scriptTaskId);
+				var form = serverWidget.createForm({ title: '報表產生中...' });
+				var summary = task.checkStatus(scriptTaskId);
+				log.debug("1");
+				if (summary.status == "PENDING" || summary.status == "PROCESSING") {
+					var mr_scripttaskid = form.addField({
+						id: 'mr_scripttaskid',
+						label: 'mr_scripttaskid',
+						type: serverWidget.FieldType.TEXT
+					});
+					log.debug("2");
+					mr_scripttaskid.updateDisplayType({ displayType: serverWidget.FieldDisplayType.HIDDEN });
+					mr_scripttaskid.defaultValue = scriptTaskId;
+					form.addSubmitButton({ label: '重新整理狀態' });
+					log.debug("3");
+					//讀取scripttype
+					var ScriptID = getScript("script", "customscript_ev_mr_gvd_export");
+					//讀取primarykey
+					var DeploymentID = getScript("scriptdeployment", "customdeploy_ev_mr_gvd_export");
+					log.debug("4");
+					log.debug("ScriptID", ScriptID);
+					log.debug("DeploymentID", DeploymentID);
+					form.addButton({
+						id: "custpage_send_ev",
+						label: "檢視進度表",
+						functionName: "window.open(\'" + util.getUrl() + "/app/common/scripting/mapreducescriptstatus.nl?sortcol=dcreated&sortdir=DESC&date=TODAY&scripttype=" + ScriptID + "&primarykey=" + DeploymentID + "\');"
+					});
+					log.debug("5");
+				} else if (summary.status == "COMPLETE") {
+					log.debug("6");
+					form.title = "報表產生已完成";
+
+					var scheduleLink = form.addField({
+						id: 'custpage_temp_1',
+						type: serverWidget.FieldType.URL,
+						label: "點選底下連結下載檔案"
+					});
+
+					var foldername = runtime.getCurrentUser().name;
+					var fileObj = file.load({
+						id: '客製報表/' + foldername + '/' + '媒體申報.txt'
+					});
+
+					scheduleLink.defaultValue = fileObj.url;
+
+				} else {
+					log.debug("7");
+					var mr_error = form.addField({
+						id: 'mr_error',
+						label: 'mr_error',
+						type: serverWidget.FieldType.TEXT
+					});
+					mr_error.defaultValue = "報表產生失敗";
+				}
+				log.debug('form', form);
+				return form;
+			} catch (error) {
+				log.debug("createStatusForm_error:" + error.name, error.message);
+			}
+		}
+
+		function getScript(type, scriptid) {
+			try {
+				var resultsid = ""
+				var scriptSearchObj = search.create({
+					type: type,
+					filters:
+						[
+							["scriptid", "is", scriptid]
+						],
+					columns:
+						[
+							search.createColumn({ name: "scriptid", label: "Script ID" })
+						]
+				});
+				scriptSearchObj.run().each(function (result) {
+					// .run().each has a limit of 4,000 results
+					resultsid = result.id;
+					return true;
+				});
+				return resultsid;
+			} catch (error) {
+				log.debug("getdepartmentSelect_error:" + error.name, error.message);
+			}
+		}
 
 		return {
 			onRequest: onRequest
